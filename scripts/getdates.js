a22469f8b5ee450c2e46fddb855e8e236ff9c4c4
RoikejuniorHaringanji <roikejunior07@gmail.com>
2025-01-14 21:38:24 +0300

    Uganda

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,2 @@
-// Update copyright year
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-
-// Update last modified date
-document.getElementById('lastModified').textContent = Last Modified: ${document.lastModified};
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("last-modified-date").textContent = document.lastModified;
