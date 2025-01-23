24ddc7adb695db829682ea1860c39b159d124cf9
RoikejuniorHaringanji <roikejunior07@gmail.com>
2025-01-23 09:30:03 +0300

    RRR

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,5 @@
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-document.getElementById("last-modified-date").textContent = document.lastModified;
+// Ensure this script is either in a separate .js file linked with 'defer' or within a <script> tag at the end of the body.
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+    document.getElementById("last-modified-date").textContent = document.lastModified;
+});
