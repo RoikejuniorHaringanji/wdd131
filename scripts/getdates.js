07671e23844f7efb601353d65d17fe4feac38d0c
RoikejuniorHaringanji <roikejunior07@gmail.com>
2025-01-25 13:21:20 +0300

    week 1 new

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,2 @@
-// Ensure this script is either in a separate .js file linked with 'defer' or within a <script> tag at the end of the body.
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById("currentyear").textContent = new Date().getFullYear();
-    document.getElementById("last-modified-date").textContent = document.lastModified;
-});
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("last-modified-date").textContent = document.lastModified;
